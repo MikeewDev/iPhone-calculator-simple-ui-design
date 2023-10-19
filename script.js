3a506ac6c2dcdd6677166be99676996187aafe52
MIKE <142926964+MikeewDev@users.noreply.github.com>
2023-10-19 12:38:11 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-document.getElementById('gerarLink').addEventListener('click', function() {
-  const ddd = document.getElementById('ddd').value;
-  const numero = document.getElementById('numero').value;
-  const mensagem = encodeURIComponent(document.getElementById('mensagem').value);
+  document.getElementById('gerarLink').addEventListener('click', function() {
+    const ddd = document.getElementById('ddd').value;
+    const numero = document.getElementById('numero').value;
+    const mensagem = encodeURIComponent(document.getElementById('mensagem').value);
 
-  const link = `https://wa.me/${ddd}${numero}?text=${mensagem}`;
+    if (ddd && numero && mensagem) {
+      const link = `https://wa.me/${ddd}${numero}?text=${mensagem}`;
+      document.querySelector('.text-link').innerHTML = `<p><a href="#" id="generatedLink">${link}</a></p>`;
+    } else {
+      alert('Please fill in all the required fields.');
+      document.querySelector('.text-link').innerHTML = '<p>Link (Click to copy)</p>';
+    }
+  });
 
-  window.location.href = link;
-});
+  document.querySelector('.text-link').addEventListener('click', function(event) {
+    if (event.target.id === 'generatedLink') {
+      const tempInput = document.createElement('input');
+      tempInput.value = event.target.textContent;
+      document.body.appendChild(tempInput);
+      tempInput.select();
+      document.execCommand('copy');
+      document.body.removeChild(tempInput);
+      alert('Link copied to clipboard.');
+    }
+  });
